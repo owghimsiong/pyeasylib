a614a247279da5f9fb9a2c46daf7ddbaa1e64c53
owghimsiong <ghimsiong.ow@gmail.com>
2024-01-25 10:30:34 +0800

    try except for dblib import
    dblib may not be used for other projects, and hence, dependencies may not be installed.

--- __init__.py
+++ __init__.py
@@ -5,4 +5,11 @@ from pyeasylib.base import *
 from pyeasylib.base_datetime import *
 import pyeasylib.pdlib as pdlib
 import pyeasylib.excellib as excellib
-import pyeasylib.dblib as dblib
+
+# Added a try except block for this, in case this is not used and the 
+# dependencies are not installed.
+try:
+    import pyeasylib.dblib as dblib
+except ModuleNotFoundError as e:
+    print (f"ModuleNotFoundError: pyeasylib.dblib not imported as: {str(e)}")
+    pass
